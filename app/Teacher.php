54da4d5ddbe85dfdb3856ba593a5d6c2dbc68e38
Travis Chen <travis0125@hotmail.com>
2018-01-07 07:46:38 +0800

    增加 laravel-ide-helper及dbal套件

--- app/Teacher.php
+++ app/Teacher.php
@@ -5,6 +5,28 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
+/**
+ * App\Teacher
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $professional
+ * @property string $url
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $employed_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher employed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereEmployedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereProfessional($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Teacher whereUrl($value)
+ * @mixin \Eloquent
+ */
 class Teacher extends Model
 {
     protected $fillable = [
--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
         /*
          * Package Service Providers...
