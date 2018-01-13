0ede4e3d6311be9265f17b8ce4dd62f27a0cc2bd
Travis Chen <travis0125@hotmail.com>
2018-01-13 08:16:50 +0800

    修改首頁內容

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,10 +88,11 @@
 
             <div class="links">
                 <hr>
+                <a href="http://www.me.lhu.edu.tw">Mechanical Engineering</a>
+                <a href="http://www.ce.lhu.edu.tw">Chemical and Materials Engineering</a>
+                <a href="http://www.ee.lhu.edu.tw">Electrical Engineering</a>
+                <a href="http://www.el.lhu.edu.tw">Electronic Engineering</a>
                 <a href="http://www.cin.lhu.edu.tw">Computer Information and Network Engineering</a>
-                <a href="http://www.el.lhu.edu.tw/">Electronic Engineering</a>
-                <a href="http://www.ee.lhu.edu.tw/">Electronical Engineering</a>
-                <a href="http://www.me.lhu.edu.tw/">Machanical Engineering</a>
             </div>
         </div>
     </div>
